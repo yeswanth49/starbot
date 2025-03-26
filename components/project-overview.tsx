d619cd29a29c98ad62b3e2328827ae7b12a5c534
yeswanth49 <sinchan123v@gmail.com>
2025-03-26 15:42:37 +0530

    added starBot and description

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -2,14 +2,9 @@ import NextLink from "next/link";
 export const ProjectOverview = () => {
   return (
     <div className="flex flex-col items-center justify-end">
-      <h1 className="text-3xl font-semibold mb-4">Vercel x Groq Chatbot</h1>
+      <h1 className="text-3xl font-semibold mb-4">starBOT</h1>
       <p className="text-center">
-        This starter project uses <Link href="https://groq.com/">Groq</Link>{" "}
-        with the <Link href="https://sdk.vercel.ai/docs">AI SDK</Link> via the{" "}
-        <Link href="https://vercel.com/marketplace/groq">
-          Vercel Marketplace
-        </Link>
-        .
+      provides everything that is necessary, resources available on PEC.UP
       </p>
     </div>
   );
